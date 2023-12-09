66b0173907a27612779637e9607315680999726d
pgdev1729 <pranav.gundu@protonmail.com>
2023-12-09 08:48:54 -0500

    update detectAprilTag.py

--- code/autonomous/ai/detectAprilTag.py
+++ code/autonomous/ai/detectAprilTag.py
@@ -1,1 +1,13 @@
 """ This is a test which is used to detect a specific april tag. """
+
+import cv2
+import apriltag
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image containing AprilTag")
+args = vars(ap.parse_args())
+print("[INFO] loading image...")
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
